8a89fac05d13467c52ed383cacbdccdf75b31ab8
Zylern <47510180+Zylern@users.noreply.github.com>
2020-06-16 10:14:30 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,10 +11,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Bot Name:- Mast User Bot**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
-                     "**Telethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
-                     "**Bot Made By:- @Zylern\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**"
-                     "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
-                     f"**My Master**: {DEFAULTUSER}\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
+    await alive.edit("◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n**Bot Name:- Mast User Bot**\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n"
+                     "**Telethon version:- 6.9.0**\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n"
+                     "**Bot Made By:- @Zylern\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n**"
+                     "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n"
+                     f"**My Master**: {DEFAULTUSER}\n◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆\n"
                      "**[Deploy this userbot Now]**(https://github.com/Zylern/MastUserBot)\n"
-                     "◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**JAI HIND🇮🇳🇮🇳 Jai Mahakaal 🙏🙏**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆")
+                     "◆ ▬▬▬▬ ❴✪❵ ▬▬▬▬ ◆")
