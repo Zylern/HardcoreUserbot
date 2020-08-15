b9f72bd926a3caa0d7d2cda86fb8ce9186d214d6
Zylern <47510180+Zylern@users.noreply.github.com>
2020-08-15 17:11:32 +0530

    Update virus_scan.py

--- userbot/plugins/virus_scan.py
+++ userbot/plugins/virus_scan.py
@@ -85,97 +85,6 @@ def time_formatter(milliseconds: int) -> str:
 
 
 
-@register(pattern=r".vtscan(?: |$)(.*)", outgoing=True)
-async def vt(event):
-    await event.edit(f"Analyzing Datas......")
-    input_str = event.pattern_match.group(1)
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-    if "|" in input_str:
-        url, file_name = input_str.split("|")
-        url = url.strip()      
-        file_name = file_name.strip()
-        head, tail = os.path.split(file_name)
-        if head:
-            if not os.path.isdir(os.path.join(TEMP_DOWNLOAD_DIRECTORY, head)):
-                os.makedirs(os.path.join(TEMP_DOWNLOAD_DIRECTORY, head))
-                file_name = os.path.join(head, tail)
-        downloaded_file_name = TEMP_DOWNLOAD_DIRECTORY + "" + file_name
-        downloader = SmartDL(url, downloaded_file_name, progress_bar=False)
-        downloader.start(blocking=False)
-        c_time = time.time()
-        display_message = None
-        while not downloader.isFinished():
-            status = downloader.get_status().capitalize()
-            total_length = downloader.filesize if downloader.filesize else None
-            downloaded = downloader.get_dl_size()
-            now = time.time()
-            diff = now - c_time
-            percentage = downloader.get_progress() * 100
-            speed = downloader.get_speed()
-            elapsed_time = round(diff) * 1000
-            progress_str = "[{0}{1}] {2}%".format(
-                ''.join(["█" for i in range(math.floor(percentage / 10))]),
-                ''.join(["░"
-                         for i in range(10 - math.floor(percentage / 10))]),
-                round(percentage, 2))
-            estimated_total_time = downloader.get_eta(human=True)
-            try:
-                current_message = f"{status}..\
-                \nURL: {url}\
-                \nFile Name: {file_name}\
-                \n{progress_str}\
-                \n{humanbytes(downloaded)} of {humanbytes(total_length)}\
-                \nETA: {estimated_total_time}"
-
-                if round(diff %
-                         10.00) == 0 and current_message != display_message:
-                    await event.edit(current_message)
-                    display_message = current_message
-            except Exception as e:
-                LOGS.info(str(e))
-        if downloader.isSuccessful():
-            await event.edit(f"{text} \n\nDownloaded  successfully !!")
-        else:
-            await event.edit("Incorrect URL\n{}".format(url))
-    elif event.reply_to_msg_id:
-        try:
-            c_time = time.time()
-            downloaded_file_name = await event.client.download_media(
-                await event.get_reply_message(),
-                TEMP_DOWNLOAD_DIRECTORY,
-                progress_callback=lambda d, t: asyncio.get_event_loop(
-                ).create_task(
-                    progress(d, t, event, c_time, f"{text} \n\nDownloading...")))
-        except Exception as e:  # pylint:disable=C0103,W0703
-            await event.edit(str(e))
-        else:
-            await event.edit(f"{text} \n\nDownloaded successfully !!")
-    else:
-        return await event.edit(f"Error\n`Reply to a file to scan.`")
-    await event.edit(" `Scanning......`")
-    vscan = downloaded_file_name
-    if a ==2:
-		      return await event.edit("`You need to Update MastUserBot to use this command.......`")
-    if not vscan:
-		     return await event.edit("`Unknown command type .help virus_scan for more info`")            
-    try:
-         vtotal = Virustotal(Vapi)
-    except:
-          return await event.edit("Failed to connect virus total , is api key added? type `.help virus_scan` for more info")
-    try:
-      vr = vtotal.file_scan(vscan)
-    except:
-      return await event.edit("`Unknown command type .help virus_scan for more info")            
-    test = vr['json_resp'] ; link = test['permalink'] ; scan_id = test['scan_id'] ; response_code = test['response_code']
-    return await event.edit(""                 
-                    f"• **Virus Total Response Code:** `{response_code}`\n"                                 
-                    f"• **Scan Results:** [ClickHere]({link}) ")
-                    
-                    
-                    
-                    
-
 @borg.on(admin_cmd(pattern=f"vtscan(?: |$)(.*)", allow_sudo=True))
 async def vt(event):
     rkp = await event.reply(f"Analyzing Datas......")
@@ -285,40 +194,7 @@ async def _(event):
           except:      
               return await event.reply(f"Please unblock @DrWebBot and try again")
 
-@borg.on(admin_cmd(pattern=f"scan(?: |$)(.*)", allow_sudo=True))
-async def _(event):
-    reply_message = await event.get_reply_message() 
-    if not reply_message or not event.reply_to_msg_id or not reply_message.media or not reply_message.media:
-       return await event.reply("```Reply to a media message```")
-    chat = "@DrWebBot"
-    sender = reply_message.sender
-    rkp = await event.reply(" `Scanning......`")
-    async with bot.conversation(chat) as conv:
-          try:        
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))                   
-              await conv.send_message(reply_message)
-              song2 = await conv.get_response()
-              return await rkp.edit(f"**MastUserBot:**  {song2.message}")
-          except:      
-              return await event.reply(f"Please unblock @DrWebBot and try again")
 
-                                
-
-@register(outgoing=True, pattern="^.uscan(?: |$)(.*)")
-async def _(event):
-       rksong = event.pattern_match.group(1)
-       if not rksong:
-            return await event.edit("`Give a link to scan.....`")
-       await event.edit(" `Scanning url.........`")
-       chat = "@DrWebBot"
-       async with bot.conversation(chat) as conv: 
-          try:        
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=161163358))                   
-              await conv.send_message(rksong)
-              song2 = await conv.get_response()
-              return await event.edit(f"**MastUserBot**:  {song2.message}")
-          except : 
-              return await event.reply("Please unblock @DrWebBot and try again")
 
                  
 @borg.on(admin_cmd(pattern=f"uscan(?: |$)(.*)", allow_sudo=True))
