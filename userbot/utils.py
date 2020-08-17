936ed6a64c755b5c60a031cfe6be252eb7e97207
Zylern <47510180+Zylern@users.noreply.github.com>
2020-08-17 07:26:57 +0000

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -1,17 +1,5 @@
 #   Copyright 2019 - 2020 DarkPrinc3
 
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 from userbot import bot
 from telethon import events
 from var import Var
@@ -22,6 +10,7 @@ from userbot import CMD_LIST
 import re
 import logging
 import inspect
+from userbot.events import *
 
 def command(**args):
     args["func"] = lambda e: e.via_bot_id is None
